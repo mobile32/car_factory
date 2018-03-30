0e2340ba4625203e4a2cc1c2aabe0d2d34f37af4
Grzegorz Zaręba <mobile443@gmail.com>
2018-03-30 17:55:14 +0200

    car_factory 1.0

--- car_park.rb
+++ car_park.rb
@@ -2,5 +2,35 @@ require './constants'
 require './car'
 
 class CarPark
-  
+  attr_reader :capacity
+  attr_reader :cars
+
+  def initialize(capacity)
+    @capacity = capacity
+    @cars = []
+  end
+
+  def receive(cars)
+    if cars.is_a? Car
+      @cars.push cars
+    elsif cars.is_a? Array
+      @cars.push *cars.first(places_left)
+    end
+  end
+
+  def retrieve(cars)
+    @cars.slice! -1, cars
+  end
+
+  def brands
+    @cars.map(&:brand).uniq
+  end
+
+  def brands_stats
+    Hash.new(0).tap { |hash| @cars.map(&:brand).each { |brand| hash[brand] += 1 } }
+  end
+
+  def places_left
+    capacity - @cars.count
+  end
 end
